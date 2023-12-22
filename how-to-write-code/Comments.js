8e1ffbce72ffd170a82ceb482e7cade1a11aede2
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2023-12-21 20:16:14 -0600

    converted readline to number

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,9 +1,12 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+//one line
 
+/* 1st line
+ then the 2nd line */
    /* Here is how
    to have
    multi-line
--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,47 @@
 console.log("Some Programming Languages:");
 
-console.log("Python\nJavaScript\nJava\nC#\nSwift");
+console.log("Python\nJavaScript\nJava\nC#\n\tSwift");
+
+
+console.log(typeof "17");
+console.log(typeof "3.2");
+console.log('The knights who say "Ni!"');
+
+console.log("Bruce's beard");
+console.log(Number("2345"));
+console.log(typeof Number("2345"));
+console.log(Number(17));
+
+console.log(Number('3'));
+console.log(Number('three'));
+console.log(Number('3 3'));
+console.log(Number('33'));
+
+let day = "Thursday";
+day = 32.5;
+day = 19;
+console.log(day);
+console.log(16 - 2 * 5 / 3 + 1);
+console.log(1 + 5 % 3);
+console.log(2 ** 2 ** 3 * 3);
+
+
+const input = require('readline-sync');
+
+let firstName = input.question("Enter your first name: ");
+let lastName = input.question("Enter your last name: ");
+console.log("Hello, " + firstName + " " + lastName + "!");
+
+//const input = require('readline-sync');
+
+let num1 = (Number(input.question("Enter a number: ")));
+let num2 = (Number(input.question("Enter another number: ")));
+
+console.log(num1 + num2);
+
+//const input = require('readline-sync');
+
+let info = input.question("Please enter your age: ");
+//The user enters 25.
+
+console.log(typeof info);
