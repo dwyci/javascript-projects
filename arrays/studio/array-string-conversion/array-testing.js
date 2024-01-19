cf5e52126c84a146342cd324c19815939bebf4d4
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-01-18 20:34:13 -0600

    difficulty with the middle part

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,12 +5,16 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+
+
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	let check = protoArray1.split(',');
+	let output = '';
 	//TODO: 2. write the code required for this step
+	output = protoArray1.reverse().join();
+
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
@@ -18,9 +22,10 @@ function reverseCommas() {
 
 //3)
 function semiDash() {
-	let check;
-	let output;
+	let check =protoArray2.split(';');
+	let output = '';
 //TODO: write the code required for this step
+output = check.sort().join('-');
 
   
 	return output;
@@ -28,20 +33,20 @@ function semiDash() {
 
 //4)
 function reverseSpaces() {
-	let check;
-	let output;
+	let check = protoArray3.split(' ');
+	let output = '';
   //TODO: write the code required for this step
 
-	return output;
+	return output = check.sort.reverse().join(' ');
 }
 
 //5)
 function commaSpace() {
-	let check;
-	let output;
+	let check = protoArray4.split(, );
+	let output = '';
 	//TODO: write the code required for this step
   
-	return output;
+	return output = check.sort.reverse.join(",");
 }
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -4,11 +4,71 @@ let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+let foodArray = food.split(",")
+let equipmentArray = equipment.split(",");
+let petsArray = equipment.split(",");
+let sleepAidsArray = sleepAids.split(",");
+
+/*console.log(foodArray);
+console.log(equipmentArray);
+console.log(petsArray);
+console.log(sleepAidsArray)*/
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
 
+/*let cargoHold = [];
+cargoHold = cargoHold.concat(foodArray,equipmentArray,petsArray,sleepAidsArray);
+console.log(cargoHold);*/
+
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
+
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+// if(cabinetNum < 0){
+//     console.log("The number you entered is invalid.");
+// } else if(cabinetNum>3){
+//     console.log("The number you entered is invalid.");
+// } else{
+//     if(cabinetNum == 0){
+//         console.log(foodArray);
+//     } else if(cabinetNum == 1){
+//         console.log(equipmentArray);
+//     } else if(cabinetNum ==2){
+//         console.log(petsArray);
+//     } else if(cabinetNum ==3){
+//         console.log(sleepAidsArray);
+//     }
+// }   
+    
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+let cabinetNum = input.question("Select a cabinet (0 - 3) in the cargoHold:  ");
+let item = input.question("And which item would you like?  ");
+if(cabinetNum == 0){
+    if(foodArray.includes(item)){
+        console.log(`Cabinet ${cabinetNum} DOES contain ${item}.`); 
+    }else{
+        console.log(`Cabinet ${cabinetNum} DOES NOT contain ${item}.`); 
+    }
+} else if(cabinetNum ==1){
+    if(equipmentArray.includes(item)){
+        console.log(`Cabinet ${cabinetNum} DOES contain ${item}.`); 
+    }else{
+        console.log(`Cabinet ${cabinetNum} DOES NOT contain ${item}.`); 
+    }
+
+}else if(cabinetNum ==2){
+    if(petsArray.includes(item)){
+        console.log(`Cabinet ${cabinetNum} DOES contain ${item}.`); 
+    }else{
+        console.log(`Cabinet ${cabinetNum} DOES NOT contain ${item}.`); 
+    }
+
+}else if(cabinetNum ==3){
+    if(sleepAidsArray.includes(item)){
+        console.log(`Cabinet ${cabinetNum} DOES contain ${item}.`); 
+    }else{
+        console.log(`Cabinet ${cabinetNum} DOES NOT contain ${item}.`); 
+    }
+}
