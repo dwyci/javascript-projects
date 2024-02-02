3107dd080f26ed214e27a61324b6dd14569e4ed5
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-02-01 20:21:19 -0600

    still working on part two

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,13 +1,42 @@
 // Code your orbitCircumference function here:
+//Define a function that returns the circumference (C = 2πr) of the orbit. Round the circumference to an integer.
 
+
+function calcCircumference(radius) {
+  let circumference = Math.round(radius*2*Math.PI);
+  return circumference;
+} 
+console.log(calcCircumference(1));
 
 // Code your missionDuration function here:
+//Define the missionDuration function to take three parameters - the number of orbits completed, 
+//the orbit radius, and the orbital speed. 
+//Set the default radius to 2000 km and the default orbital speed to 28000 km/hr.
+//For example, with the default radius and speed, 5 orbits will take about 2.24 hours.
+
+let radiusDefault = 2000;
+let orbitalSpeedDefault = 28000;
+
+let missionDuration= (numOfOrbits, orbitRadius = radiusDefault, orbitalSpeed= orbitalSpeedDefault) =>{
+
+  let time = ((calcCircumference(orbitRadius)*numOfOrbits)/orbitalSpeed).toFixed(2);
+  return time;
+}
+//'The mission will travel ____ km around the planet, and it will take ____ hours to complete.'
+console.log(`The mission will travel ${calcCircumference(radiusDefault)} km around the planet, and it will take ${missionDuration(5)} hours to complete.`)
+
 
 
 // Copy/paste your selectRandomEntry function here:
+function selectRandomEntry(arr){
+  let index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+} 
 
 
 // Code your oxygenExpended function here:
+// When you call `oxygenExpended`, feel free to use your`selectRandomEntry` to pick the crew member to pass into the function.
+
 
 
 // Candidate data & crew array.
