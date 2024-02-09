65aec6ba26599fc2b2e8a279fddc3e5d6bcc3a6c
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-02-08 22:02:57 -0600

    studio

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,27 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test(" check that the value of organization is nonprofit", function(){
+    expect(launchcode.organization).toBe("nonprofit");
+  });
 
-  // Write your unit tests here!
+  test(" value of executiveDirector is Jeff" , function(){
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("  value of percentageCoolEmployees ", function(){
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+//programsOffered with a value of ["Web Development", "Data Analysis", "Liftoff"]
+  test("  check appropriate values in array and number in array ", function(){
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+
+  });
+
+ 
   
 });
