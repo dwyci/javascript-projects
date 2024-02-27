162516827cc39a33ebfcf5e46d0b08234b076080
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-02-27 10:16:11 -0600

    added mouseover, moueout, confirm,and click features

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,26 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', event =>{
+        paragraph.innerHTML = 'Houston!  We have lift off!';
+    });
+
+    missionAbort.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = 'red';
+     });
+     
+     missionAbort.addEventListener('mouseout', event => {
+        event.target.style.backgroundColor = ''; 
+     });
+
+     missionAbort.addEventListener('click', event=>{
+            const confirmation = confirm('Are you sure you want to abort the mission?');
+            if(confirmation){
+                paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+            }
+            
+     });
     
 }
 
-window.addEventListener("load", init);
+
